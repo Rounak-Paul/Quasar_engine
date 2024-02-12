e43c0d71ef9d52e40dfdd37cce0756d9088d3487
Rounak Paul <rounakpaul@hotmail.com>
2024-02-13 00:19:41 +0530

    a

--- Quasar/src/Renderer/Renderer.h
+++ Quasar/src/Renderer/Renderer.h
@@ -14,6 +14,7 @@ namespace Quasar {
         private:
         void Init();
         VkInstance instance;
+        VkDebugUtilsMessengerEXT debugMessenger;
 
         const std::vector<const char*> validationLayers = {
             "VK_LAYER_KHRONOS_validation"
@@ -25,6 +26,17 @@ namespace Quasar {
             const bool enableValidationLayers = false;
         #endif
 
+        void CreateInstance();
         bool CheckValidationLayerSupport();
+        std::vector<const char*> GetRequiredExtensions();
+        void SetupDebugMessenger();
+        void PopulateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+        static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+            std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+            return VK_FALSE;
+        }
+        VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
+        void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
     };
 }
